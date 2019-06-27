be4175fadecb9ee2aa4455d8b9e2ed20e7ae5761
Nickolay Nickitin <ndnickitin@gmail.com>
2019-06-27 22:55:54 +0300

    init commit

---
+++ main.swift
@@ -0,0 +1,68 @@
+//
+//  main.swift
+//  testic
+//
+//  Created by Nickolay Nickitin on 26/06/2019.
+//  Copyright © 2019 Nickolay Nickitin. All rights reserved.
+//
+
+import Foundation
+
+class Car {
+    
+    let model: String
+    let mark: String
+    let color: String
+    var position: Car.Coordinate
+    
+    enum Orientation {
+        case up
+        case down
+        case rigth
+        case left
+    }
+    
+    struct Coordinate {
+        var x : Int
+        var y : Int
+    }
+    
+    init(model: String, mark: String, color: String, position: Car.Coordinate = Car.Coordinate(x: 0, y: 0)) {
+        self.model = model
+        self.mark = mark
+        self.color = color
+        self.position = position
+    }
+    
+    func move(direction: Car.Orientation, time: Int, speed: Int) {
+        let distance = time * speed
+        switch direction {
+        case .up:
+            position.x += distance
+        case .down:
+            position.x -= distance
+        case .rigth:
+            position.y += distance
+        case .left:
+            position.y -= distance
+        }
+        print("distance = \(distance), x = \(position.x), y = \(position.y)")
+    }
+    
+    func execute(action: Go) {
+        self.move(direction: action.direction, time: action.time, speed: action.speed)
+    }
+    
+}
+
+struct Go {
+    let direction : Car.Orientation
+    let time : Int
+    let speed : Int
+}
+
+let car = Car(model: "Vaz", mark: "2107", color: "Cherry")
+
+car.execute(action: Go(direction: .up, time: 10, speed: 15))
+car.execute(action: Go(direction: .up, time: 7, speed: 4))
+car.execute(action: Go(direction: .rigth, time: 20, speed: 10))
